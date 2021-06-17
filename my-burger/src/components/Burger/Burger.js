82a674dd2a85830cb014322ed84539bba5b3280f
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-06-17 07:23:41 +0200

    implemented add/remove ingredient logic

--- my-burger/src/components/Burger/Burger.js
+++ my-burger/src/components/Burger/Burger.js
@@ -6,7 +6,7 @@ const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i) =>{
-            return <BurgerIngredient key={igKey + 1} type={igKey}/>
+            return <BurgerIngredient key={igKey + i} type={igKey}/>
         });
     })
     .reduce((arr,el) => {
@@ -18,8 +18,6 @@ const burger = (props) => {
         <p>Please start adding ingredients!</p>
     }
     
-
-
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
